ddacd978bcca1edb4ea5b8040da661cff857c0b1
James Wallace <4538317+nzlwally@users.noreply.github.com>
2020-06-15 22:42:33 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -1,46 +1,43 @@
+# Helper Method
 def position_taken?(board, index)
   !(board[index].nil? || board[index] == " ")
 end
 
 # Define your WIN_COMBINATIONS constant
 WIN_COMBINATIONS = [
-  [0, 1, 2],
-  [3, 4, 5],
-  [6, 7, 8],
-  [0, 3, 6],
-  [1, 4, 7],
-  [2, 5, 8],
-  [0, 4, 8],
-  [2, 4, 6]
+  [0,1,2],
+  [3,4,5],
+  [6,7,8],
+  [0,3,6],
+  [1,4,7],
+  [2,5,8],
+  [0,4,8],
+  [6,4,2]
 ]
 
+# Define won?, full?, draw?, over?, and winner below
 def won?(board)
-  if board.all? { |element| element == ' ' }
-    return false
-  else 
-    WIN_COMBINATIONS.each do |combination|
-      if combination.all? { |element| board[element] == 'X' }
-        return combination
-      elsif combination.all? { |element| board[element] == 'O' }
-        return combination
-      end
-    end
+  WIN_COMBINATIONS.detect do |combo|
+    board[combo[0]] == board[combo[1]] &&
+    board[combo[1]] == board[combo[2]] &&
+    position_taken?(board, combo[0])
   end
-  return false
 end
 
 def full?(board)
-  board.none? { |element| element == ' ' }
+  board.all?{|token| token == "X" || token == "O"}
 end
 
 def draw?(board)
-  !won?(board) && full?(board)
+  full?(board) && !won?(board)
 end
 
 def over?(board)
-  won?(board) || draw?(board)
+  won?(board) || full?(board)
 end
 
 def winner(board)
-  won?(board) ? board[won?(board)[0]] : nil
+  if winning_combo = won?(board)
+    board[winning_combo.first]
+  end
 end
